6cb387529d37311f22d42c44b2f7425106307e8d
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-06 00:27:22 -0500

    Added age and years active  artists search query

--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,20 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  const minYearsActive = Artist
+      .find({})
+      .sort({ yearsActive: 1 })
+      .limit(1)
+      .then((artist) => artist[0].yearsActive);
+
+  const maxYearsActive = Artist
+      .find({})
+      .sort({ yearsActive: -1 })
+      .limit(1)
+      .then((artist) => artist[0].yearsActive);
+
+  return Promise.all([minYearsActive, maxYearsActive])
+      .then((years) => {
+      return { min: years[0], max: years[1] };
+  });
 };
