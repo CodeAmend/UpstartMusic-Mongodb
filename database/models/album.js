ad0c8e608e96c8712946c13b233429a9d7a38fd8
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-05 21:13:32 -0500

    Created Album and Artist schema with Mongoose

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,16 @@
-// Todo: create Album Schema
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AlbumSchema = Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number
+});
+
+const Album = mongoose.models('album', AlbumSchema);
+
+module.exports = Album;
--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,21 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const Album = require('./album');
+
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  album: [Album]
+});
+
+const Artist = mongoose.models('artist', ArtistSchema);
+
+module.exports = Artist;
